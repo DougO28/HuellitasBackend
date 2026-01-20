042895450106e4af7ac0d976925c71777695e42f
DougO28 <douguiortegaq@gmail.com>
2026-01-20 10:11:50 -0600

    Configuracion de URL de Frontend para API

--- core/settings/production.py
+++ core/settings/production.py
@@ -69,6 +69,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:5173',
+    'https://huellitasrefugio.netlify.app',
 ]
 
 # Agregar origins de producción desde variables de entorno
